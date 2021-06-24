1232181ecff2cdb52290d7bc540a8e9ccf823531
AriesxBackup <73474200+AriesxBackup@users.noreply.github.com>
2021-06-24 11:46:41 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,1 @@
-termux-setup-storage && pkg update -y && pkg upgrade && pkg install python -y && pkg install pv -y && pip install lolcat && pkg install wget -y && pkg install nodejs -y && npm install --global internal-ip-cli
+termux-setup-storage && pkg install python -y && pkg install pv -y && pip install lolcat && pkg install wget -y && pkg install nodejs -y && npm install --global internal-ip-cli && pkg install cmatrix
