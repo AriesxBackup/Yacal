59fe647b2317775fb1a25d983a4312a16a86271a
AriesxBackup <73474200+AriesxBackup@users.noreply.github.com>
2021-06-24 11:23:24 +0530

    Add files via upload

---
+++ YACAL-TEST.sh
@@ -0,0 +1,126 @@
+date "+%a %b %d %H:%M:%S %Z %Y" | lolcat | pv -qL 12000
+echo -e "━━━━▣━━◤◢━━▣━━━━━" | lolcat | pv -qL 120000
+echo
+echo " Uꜱᴇʀ Nᴀᴍᴇ :" | lolcat | pv -qL 1200
+read user
+sleep 1
+echo
+echo " Pᴀꜱꜱᴡᴏʀᴅ :" | lolcat | pv -qL 1200
+read pass
+echo
+echo -e "━━━━▣━━◤◢━━▣━━━━━" | lolcat | pv -qL 120000
+if [[ $user == "Aries"  && $pass == "Kaztro" ]]
+then
+sleep 1
+clear
+echo
+echo -ne " ╭━┳╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━┳━┳━━╮╱╱╭╮"  | lolcat | pv -qL 3000000
+sleep 0.5
+echo -ne " ╰╮┃┣━╮╭╮╭━┳━┳━╮╭╮╭━┫━┫━┫╭━╋━┳╯┃"  | lolcat | pv -qL 3000000
+echo -ne " ╭┻╮┃╋╰┫╰┫╋┃━┫╋╰┫╰┫╋┃╭┫╭┫╰╮┃╋┃╋┃"  | lolcat | pv -qL 3000000
+sleep 0.3
+echo -ne " ╰━━┻━━┻━┻━┻━┻━━┻━┻━┻╯╰╯╰━━┻━┻━╯ "  | lolcat | pv -qL 3000000
+sleep 0.4
+echo
+echo -e " ╔─━━━━  ©  ━━━━━─╗" | lolcat | pv -qL 12000
+echo -e "   𝘾𝙧𝙚𝙖𝙩𝙤𝙧: @YalocaloffGod" | lolcat | pv -qL 120
+echo -e " ╚─━━━━  ©  ━━━━━─╝" | lolcat | pv -qL 12000
+echo -e "
+ Tʜɪꜱ ᴛᴏᴏʟ ɪꜱ ᴛᴏ ᴜꜱᴇ " | lolcat | pv -qL 12000
+echo -e " Aɴᴅ ɴᴏᴛ ᴍᴇᴀɴᴛ ᴛᴏ ʙᴇ ꜱᴏʟᴅ ɪɴ ᴀɴʏ ꜰᴏʀᴍ
+" | lolcat | pv -qL 12000
+sleep 1
+echo -e " ▌▌║█│▌║ ᴾᵁᴮᴳ ᴹᴼᴮᴵᴸᴱ [ ¹³⁷⁷† ]║▌║▌║▌║█" | lolcat | pv -qL 12000
+echo -e ""
+date "+%a %b %d %H:%M:%S %Z %Y" | lolcat | pv -qL 12000
+sleep 1
+echo
+echo -e "   Fᴏʀ Hᴀᴛᴇʀꜱ ɪ Mᴀᴋᴇ Tʜɪɴɢꜱ Yᴏᴜ Nᴇᴠᴇʀ Iᴍᴀɢɪɴᴇᴅ " | lolcat | pv -qL 300
+echo
+function menu {
+echo
+echo -e " Wᴇʟᴄᴏᴍᴇ Nᴇᴡ Uꜱᴇʀ: " $(( ( RANDOM % 2 )  + 1 )) | lolcat | pv -qL 300
+echo
+echo -e "   | 𝙁𝙪𝙣𝙘𝙩𝙞𝙤𝙣 ��𝙚��𝙪 |  
+" | lolcat | pv -qL 20000
+echo -e " 1. 𝑩𝑮𝑴𝑰 %𝑷𝑹𝑶𝑪  " | lolcat | pv -qL 20000
+echo -e " 2. 𝑳𝒐𝒃𝒃𝒚 %𝑷𝑹𝑶𝑪  " | lolcat | pv -qL 20000
+echo -e " 3. 𝑹𝒆𝒔𝒕𝒐𝒓𝒆 %𝑷𝑹𝑶𝑪  " | lolcat | pv -qL 20000
+echo -e " 0. Exɪᴛ Mᴇɴᴜ" | lolcat | pv -qL 20000
+echo -en "
+    Fᴜɴᴄᴛɪᴏɴ : " | lolcat | pv -qL 12000
+read -n 1 option
+}
+while [ 1 ]
+do
+menu
+case $option in
+0)
+break ;;
+1)
+echo
+su
+echo -e ""
+RAM=`grep "MemTotal" /proc/meminfo | cut -f 2 -d ':'`
+sleep 1
+echo " RAM : " $RAM| lolcat | pv -qL 12000
+sleep 0.4
+echo "           🄸🄽🄸🅃🄸🄰🄻🄸🄽🄶 🄱🅈🄿🄰🅂🅂 " | lolcat | pv -qL 20000
+sleep 0.5
+#before Opening Game
+sleep 2
+rm -rf /storage/emulated/0/.backups &>/dev/null 
+mkdir /storage/emulated/0/.backups &>/dev/null 
+chmod 000 /data/media/0/.backups &>/dev/null 
+rm -rf /data/data/com.pubg.imobile/libgamemaster.so &>/dev/null 
+chmod 404 /data/data/com.pubg.imobile/libcubehawk.so &>/dev/null
+echo " G̴͙͓͉͌͒̚A̸̡̞͛̐̀M̵͍͖͓͆̔͒Ë̵̢̦͕́̾͘ O̸̢̡̪͋͋̐P̵̢͓̙̈́̓͠E̵̟͙͔̾̽̕N̵͕̼̽̚͝I̸͙͕͓̐̈́̚N̴͇͖͐͋̕G̸̠̪͒̓́͜!̵͉̞̪̒͆͝!̴̫͕̺͐̔̚ " | lolcat | pv -qL 20000
+
+#DIRECTORY DEFINE
+cp /data/data/com.pubg.imobile/lib /storage/downloads/
+am start -n com.pubg.imobile/com.epicgames.ue4.SplashActivity &> /dev/null
+sleep 4
+rm -rf /data/data/com.pubg.imobile/lib/libUE4.so
+rm -rf /data/data/com.pubg.imobile/lib/libtprt.so
+rm -rf /data/data/com.pubg.imobile/lib/libtersafe.so
+rm -rf /data/data/com.pubg.imobile/lib/libTDataMaster.so
+cp /data/data/com.termux/files/home/Yacal/lib/libTDataMaster.so /data/data/com.pubg.imobile/lib/
+cp /data/data/com.termux/files/home/Yacal/lib/libtersafe.so /data/data/com.pubg.imobile/lib/
+cp /data/data/com.termux/files/home/Yacal/lib/libtprt.so /data/data/com.pubg.imobile/lib/
+cp /data/data/com.termux/files/home/Yacal/lib/libUE4.so /data/data/com.pubg.imobile/lib/
+chmod 755 /data/data/com.pubg.imobile/lib/libUE4.so
+chmod 755 /data/data/com.pubg.imobile/lib/libtprt.so
+chmod 755 /data/data/com.pubg.imobile/lib/libtersafe.so
+chmod 755 /data/data/com.pubg.imobile/lib/libTDataMaster.so
+sleep 1 ;;
+2)
+sleep 2
+chmod 4000 /data/data/com.pubg.imobile/databases &>/dev/null 
+chmod 4000 /data/data/com.pubg.imobile/databases/* &>/dev/null 
+echo " DONE IN LOBBY " | lolcat | pv -qL 20000
+sleep 1 ;;
+3)
+#RESTORE GAME
+cp /storage/downloads/lib /data/data/com.pubg.imobile/
+echo " Done " | lolcat | pv -qL 20000
+sleep 1 ;;
+*)
+echo
+echo " Sᴏʀʀʏ Bᴜᴛ Tʜᴇ Iɴᴘᴜᴛ Wᴀꜱ Iɴᴠᴀʟɪᴅ
+ Pʟᴇᴀꜱᴇ Sᴇʟᴇᴄᴛ ᴀ Vᴀʟɪᴅ Iɴᴘᴜᴛ ";;
+esac
+echo -en " Hɪᴛ Aɴʏ Kᴇʏ Tᴏ Cᴏɴᴛɪɴᴜᴇ " | lolcat | pv -qL 18000
+read -n 1 line
+done
+echo
+echo -e "     ╭━─━─━─━─━≪✠≫━─━─━─━─━╮" | lolcat | pv -qL 120000
+echo -e "      ��𝙝��𝙣��𝙚�� : @YalocaloffGod " | lolcat | pv -qL 95
+echo -e "     ╰━─━─━─━─━≪✠≫━─━─━─━─━╯" | lolcat | pv -qL 120000
+elif [ $user != "xcode" ]
+then
+echo -e "Wʀᴏɴɢ Uꜱᴇʀɴᴀᴍᴇ [×] 
+" | lolcat | pv -qL 800
+else
+echo -e "Wʀᴏɴɢ Pᴀꜱꜱᴡᴏʀᴅ [×] 
+" | lolcat | pv -qL 800
+fi
